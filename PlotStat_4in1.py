deffe360ab2079db813c9b518af2cc50da2f4a1c
Bingjun Zhang <bingjunzhang@gmail.com>
2012-08-17 14:49:43 -0500

    REF: plot with fitness cutoff

--- PlotStat_4in1.py
+++ PlotStat_4in1.py
@@ -11,6 +11,7 @@ os.chdir(data_path)
 dirs = glob.glob("Core_Expo_M0.0_R0.0_G600000_N1000_BeneMR3.0E-5_DeleMR0.05_BeneE0.03_DeleE0.03_MutStr2_MutMR1.0E-4_AntiMutMR1.0E-5_MutaE0.03")
 print(os.getcwd())
 print(len(dirs))
+fitness_cutoff = 1e-10
 
 nG_exp=re.compile(r"_G(?P<nG>\d+)_")
 mu_exp=re.compile(r"DeleMR(?P<mu>[\d\.E-]+)_")
@@ -36,11 +37,13 @@ for dir_name in dirs:
         file.next()
         for line in file:
             if line:
-                fitness.append(line[1])
-                mutator_strength.append(line[3])
-                n_dele.append(line[5])
-                n_bene.append(line[7])
-
+                curr_fitness = float(line[1])
+                if curr_fitness >= fitness_cutoff:
+                    fitness.append(line[1])
+                    mutator_strength.append(line[3])
+                    n_dele.append(line[5])
+                    n_bene.append(line[7])
+                else: break
 #        print n_bene
         fitness = string_to_float_list(fitness)
         mutator_strength = string_to_float_list(mutator_strength)
@@ -49,27 +52,30 @@ for dir_name in dirs:
 
 #        print n_bene
 
-
+        x_ax = [i * 1000 for i in range(1,len(fitness)+1)]
+#        print x_ax, len(fitness)
+#        print fitness, fitness_cutoff
+#        exit(0)
         fig = plt.figure(figsize=(20, 18))
         plt.subplot(221)
-        plt.plot(gen_counts, fitness)
+        plt.plot(x_ax, fitness)
         plt.xlabel("Generation", fontsize=20)
         plt.ylabel("Fitness", fontsize=20)
 
         plt.subplot(222)
-        plt.plot(gen_counts, mutator_strength)
+        plt.plot(x_ax, mutator_strength)
         plt.xlabel("Generation", fontsize=20)
         plt.ylabel("Mutator Strength", fontsize=20)
 
         plt.subplot(223)
-        plt.plot(gen_counts, n_bene)
+        plt.plot(x_ax, n_bene)
         plt.xlabel("Generation", fontsize=20)
         plt.ylabel("# of Beneficial Mutations", fontsize=20)
 
         plt.subplot(224)
-        plt.plot(gen_counts, n_dele)
+        plt.plot(x_ax, n_dele)
         plt.xlabel("Generation", fontsize=20)
         plt.ylabel("# of Deleterious Mutations", fontsize=20)
 
-        fig.savefig(file_name + ".png")
+        fig.savefig("FitCutoff" + file_name + ".png")
 
